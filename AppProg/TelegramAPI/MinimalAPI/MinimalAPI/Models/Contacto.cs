a9f07ed7fad57494b5055808d16317bac331c042
Julio-Contesse <101760161+Julio-Contesse@users.noreply.github.com>
2022-10-31 19:50:59 -0300

    Add files via upload

---
+++ AppProg/TelegramAPI/MinimalAPI/MinimalAPI/Models/Contacto.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPI.Models
+{
+    public class Contacto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Fono { get; set; }
+    }
+}
