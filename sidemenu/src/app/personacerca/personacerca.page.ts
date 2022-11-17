93f9e985a5e2a2971bcfce4ae5b706d0e6e7b45d
VallRax <101760319+VallRax@users.noreply.github.com>
2022-11-17 13:23:06 -0300

    7.4, Geolocalizacion, Personas Cercas

--- sidemenu/src/app/personacerca/personacerca.page.ts
+++ sidemenu/src/app/personacerca/personacerca.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-personacerca',
@@ -7,7 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonacercaPage implements OnInit {
 
-  constructor() { }
+  constructor( public geolocation: Geolocation) { 
+    this.getGeolocation();
+   }
+
+  lat: number;
+  lon:number;
+  total: string;
+
+  getGeolocation(){
+    this.geolocation.getCurrentPosition().then((geoposition: Geoposition) =>{
+      this.lat = geoposition.coords.latitude;
+      this.lon = geoposition.coords.longitude;
+
+      let latSanjoaquin = -33.49972601464;
+      let lonSanjoaquin = -70.6163849865586;
+
+      this.total = this.calculateDistance(this.lon, lonSanjoaquin, this.lat, latSanjoaquin) + "KM";
+    });
+  }
+  
+  calculateDistance(lon1, lon2, lat1, lat2){
+    let p = 0.017453292519943295;
+    let c = Math.cos;
+    let a = 0.5 - c((lat1-lat2) * p) / 2 + c(lat2 * p) *c((lat1) * p) * (1 - c(((lon1- lon2) * p))) / 2;
+    let dis = (12742 * Math.asin(Math.sqrt(a)));
+    return Math.trunc(dis);
+}
+
+
 
   ngOnInit() {
   }
